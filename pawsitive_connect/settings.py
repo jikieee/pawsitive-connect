76491c93985cd085db2b223f470b9a65f3f0ccd9
Jacob <jikjikpaje@gmail.com>
2026-07-08 22:57:49 +0800

    Configure PythonAnywhere allowed hosts

--- pawsitive_connect/settings.py
+++ pawsitive_connect/settings.py
@@ -7,7 +7,11 @@ SECRET_KEY = 'django-insecure-pawsitive-connect-secret-key-change-in-production'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["Cols.pythonanywhere.com", "localhost", "127.0.0.1"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://Cols.pythonanywhere.com",
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
